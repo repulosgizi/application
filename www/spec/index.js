b5ae3821a9b89612fa8380930ae0453f43788c7b
Edina Hoffmann <edinahoffmann@gmail.com>
2018-08-20 15:29:05 +0200

    forgatás javítása, de még csak class-al megy

--- www/spec/index.js
+++ www/spec/index.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 describe('app', function() {
     describe('initialize', function() {
         it('should bind deviceready', function() {
@@ -67,18 +50,16 @@ describe('app', function() {
 });
 
 
- /*image rotate jquery*/
-    $(document).ready(function(imgRotate) {
+/*image rotate jquery*/
+   /* $(document).ready(function(imgRotate) {
 
 
       var degrees = 0;
-      $('#imgRotation1'&'#imgRotation5').click(function rotateMe(e) {
+      $('#imgRotation1').click(function rotateMe(e) {
 
         degrees += 90;
 
-        //$('.img').addClass('rotated'); // for one time rotation
-
-        $('#img1'&'#img2'&'#img3'&'#img4'&'#img5'&'#img6'&'#img7'&'#img8'&'#img9'&'#img10'&'#img11'&'#img12'&'#img13'&'#img14'&'#img15').css({
+        $('.img1').css({
           'transform': 'rotate(' + degrees + 'deg)',
           '-ms-transform': 'rotate(' + degrees + 'deg)',
           '-moz-transform': 'rotate(' + degrees + 'deg)',
@@ -87,13 +68,32 @@ describe('app', function() {
         });
   })
 
-       
-     
+    });*/
+
+   $(document).ready(function() {
 
 
+  var degrees = 0;
+  $('.imgRotation1').click(function rotateMe(e) {
 
+    degrees += 90;
 
+    //$('.img').addClass('rotated'); // for one time rotation
 
+    $('.img').css({
+
+      'transform': 'rotate(' + degrees + 'deg)',
+      '-ms-transform': 'rotate(' + degrees + 'deg)',
+      '-moz-transform': 'rotate(' + degrees + 'deg)',
+      '-webkit-transform': 'rotate(' + degrees + 'deg)',
+      '-o-transform': 'rotate(' + degrees + 'deg)'
     });
+
+  })
+
+
+
+
+});
 
   /*image rotate jquery end*/
